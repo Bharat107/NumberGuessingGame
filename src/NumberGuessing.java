f1029cac86fe49ea05de3c74431a9dc9700a3c3a
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-03-21 08:26:30 -0400

    Added a few variables

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class NumberGuessing {
+
+   public static void main(String[] args){
+
+       Scanner keyboard;
+       int pickedNumber;
+       int playerGuess;
+       int turnsAvailable;
+       String playerChoice;
+
+       
+
+
+
+   }
+
 }
