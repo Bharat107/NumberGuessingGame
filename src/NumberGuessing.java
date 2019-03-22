0ff2237bb244995d7dc4fe718a54e41dd3801393
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-03-22 07:51:17 -0400

    Added what happens when you guess the correct number

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -7,8 +7,30 @@ public class NumberGuessing {
        Scanner keyboard;
        int pickedNumber;
        int playerGuess;
-       int turnsAvailable;
+       int playerTurns;
        String playerChoice;
+
+       boolean play = true;
+
+       keyboard = new Scanner(System.in);
+
+       do {
+           playerTurns = 10;
+           boolean gameWon = false;
+           int totalTries = 0;
+           pickedNumber = (int) (Math.random() * 100);
+           System.out.println("Guess a number that's between 1-100.");
+           while (!gameWon && totalTries < playerTurns) {
+               playerGuess = keyboard.nextInt();
+               totalTries++;
+               if (playerGuess == pickedNumber) {
+                   System.out.println("You won!");
+                   System.out.println("It took you " + totalTries + " turns to guess my number, which was " + pickedNumber + "!");
+                   System.out.println("Thank you for playing.");
+                   gameWon = true;
+               }
+           }
+       }
 
        
 
